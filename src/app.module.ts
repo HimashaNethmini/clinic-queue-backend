8b51f61326bd9846e1429373bd0090029c34f80e
HimashaNethmini <himashanethmini05@gmail.com>
2026-05-09 12:34:36 +0530

    created doctor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServiceModule } from './doctor/service/service.module';
+import { DoctorService } from './doctor/doctor.service';
+import { DoctorController } from './doctor/doctor.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ServiceModule],
+  controllers: [AppController, DoctorController],
+  providers: [AppService, DoctorService],
 })
 export class AppModule {}
