baff6ce269e42bf5c26b197abaefacc4cca660e1
HimashaNethmini <himashanethmini05@gmail.com>
2026-05-09 15:03:49 +0530

    installed validator

--- src/doctor/dto/create-doctor.dto.ts
+++ src/doctor/dto/create-doctor.dto.ts
@@ -1,15 +1,26 @@
 import { DoctorStatus, Specialization } from '@prisma/client';
+import {
+  IsEmail,
+  IsEnum,
+  IsString,
+} from 'class-validator';
 
 export class CreateDoctorDto {
+  @IsString()
   name!: string;
 
+  @IsEmail()
   email!: string;
 
+  @IsString()
   phone!: string;
 
+  @IsEnum(Specialization)
   specialization!: Specialization;
 
+  @IsString()
   availableTime!: string;
 
-  status?: DoctorStatus;
+  @IsEnum(DoctorStatus)
+  status!: DoctorStatus;
 }
